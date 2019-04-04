52887453744ca00f05f800d0c0a715dfc544c241
jsoriaso7 <jlsoriano@hotmail.es>
2019-04-04 10:25:59 +0200

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -3,6 +3,8 @@
         groupIdLocal=$1
         artifactIdLocal=$2
         majorMinorLocal=$3
+	username=$4
+	pwd=$5
 
         groupIdPath=${groupId//.//}
 
@@ -14,8 +16,9 @@
         echo $groupIdLocal
         echo $artifactIdLocal
         echo $majorMinorLocal
-
-	DATOS=$(curl -u admin:Deloitte01 "http://localhost:8081/artifactory/api/search/versions?g=$groupIdLocal&a=$artifactIdLocal&repos=libs-release-local")
+	echo $username
+	
+	DATOS=$(curl -u "${username}:${pwd}" "http://localhost:8081/artifactory/api/search/versions?g=$groupIdLocal&a=$artifactIdLocal&repos=libs-release-local")
         #DATOS="${curl http://localhost:8081/artifactory/api/search/versions?g=$groupIdLocal&a=$artifactIdLocal&repos=libs-release-local}"
 
         echo $DATOS
@@ -30,7 +33,7 @@
                 }
 
                 version_to_remove=$(echo $(_jq '.version'))
-                curl -i -X DELETE -u admin:Deloitte01 'http://localhost:8081/artifactory/libs-release-local/'$groupIdPath'/'$artifactId'/'$version_to_remove
+                curl -i -X DELETE -u "${username}:${pwd}" 'http://localhost:8081/artifactory/libs-release-local/'$groupIdPath'/'$artifactId'/'$version_to_remove
         done
 
 
